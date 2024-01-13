8397ea1f3caddb8d7514611aedee77a60d2ac370
James Koga <jydkoga@gmail.com>
2024-01-12 17:36:23 -0800

    init commit

---
+++ src/lib/index.js
@@ -0,0 +1,48 @@
+// place files you want to import through the `$lib` alias in this folder.
+// ?client_id = 118455 & client_secret=01dfec1b65ed43a13e21fd4cd5b57887708eea67 & grant_type=refresh_token & refresh_token=4580e6ec0472324d897fb45eab904b510b2cda73
+
+const auth_link = "https://www.strava.com/api/v3/oauth/token"
+
+/**
+ * @param {{ access_token: any; }} res
+ */
+function getActivities(res) {
+  const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+  fetch(activities_link)
+    .then((res) => res.json())
+    .then(
+      function (data) {
+        // 		var map = L.map('map').setView([51.505, -0.09], 13);
+        // 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        // 			maxZoom: 19,
+        // 			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // 		}).addTo(map);
+
+        for (var x = 0; x < data.length; x++) {
+          console.log(data[x])
+        }
+      }
+    )
+}
+
+
+function reAuthorize() {
+  fetch(auth_link, {
+    method: "post",
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+
+    body: JSON.stringify({
+      client_id: '118455',
+      client_secret: '01dfec1b65ed43a13e21fd4cd5b57887708eea67',
+      refresh_token: '4580e6ec0472324d897fb45eab904b510b2cda73',
+      grant_type: 'refresh_token'
+
+    })
+  }).then(res => res.json())
+    .then(res => getActivities(res))
+}
+
+reAuthorize()
