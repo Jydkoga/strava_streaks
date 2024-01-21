e78fa6b847ed3525e1690ee861e1fec526fb9d1b
James Koga <jydkoga@gmail.com>
2024-01-21 17:13:01 -0500

    Fixed checking with start date. Also added HTML output for total distance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
-  const auth_link = "https://www.strava.com/api/v3/oauth/token";
+  // @ts-nocheck
 
+  import { browser } from "$app/environment";
+
+  const auth_link = "https://www.strava.com/api/v3/oauth/token";
+  const streak_start_date = "2024-01-01";
+  var filtered_data = [];
+  var totalDist = 0;
   /**
    * @param {{ access_token: any; }} res
    */
@@ -14,11 +20,45 @@
         // 			maxZoom: 19,
         // 			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         // 		}).addTo(map);
-
-        for (var x = 0; x < data.length; x++) {
-          console.log(data[x]);
+        filtered_data = data.filter(
+          // @ts-ignore
+          (activity) =>
+            compareDate(activity.start_date, streak_start_date) == 1,
+        );
+        for (var x = 0; x < filtered_data.length; x++) {
+          console.log(filtered_data[x]);
+          totalDist += filtered_data[x].distance;
+        }
+        console.log("total distance (in Miles): " + getMiles(totalDist));
+        console.log("total distance (in Meters): " + totalDist);
+        if (browser) {
+          document.getElementById("dist").innerHTML =
+            "total distance (in Miles): " +
+            Math.round(((getMiles(totalDist) + Number.EPSILON) * 100) / 100);
         }
       });
+  }
+  function formatDate(s) {
+    var year = s.substring(0, 4);
+    var month = s.substring(5, 7);
+    var day = s.substring(8, 10);
+    return month + "/" + day + "/" + year;
+  }
+  /** CompareDate(s1,s2) returns -1 if s2>s1, and 1 if s1>s2. 0 otherwise */
+  function compareDate(s1, s2) {
+    let date1 = new Date(formatDate(s1)).getTime();
+    let date2 = new Date(formatDate(s2)).getTime();
+    if (date1 < date2) {
+      return -1;
+    } else if (date1 == date2) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
+  function getMiles(meters) {
+    return meters * 0.000621371192;
   }
 
   function reAuthorize() {
@@ -44,4 +84,6 @@
 </script>
 
 <head></head>
-<body>Hello World! </body>
+<body>
+  <p id="dist"></p>
+</body>
